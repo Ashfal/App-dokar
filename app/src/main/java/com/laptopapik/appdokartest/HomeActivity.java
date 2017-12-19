52cc6258b02927269da61dc9fe373040e97206dd
Ashfal <ashfal.fuady@gmail.com>
2017-12-19 15:10:34 +0700

    Git First Update

---
+++ app/src/main/java/com/laptopapik/appdokartest/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.laptopapik.appdokartest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+
+}

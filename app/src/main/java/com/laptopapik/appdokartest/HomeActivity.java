bf970bcb59cb7fa82f38b8bd5ccb04c4a6792723
Ashfal <ashfal.fuady@gmail.com>
2017-12-20 09:56:33 +0700

    Git Second Update

--- app/src/main/java/com/laptopapik/appdokartest/HomeActivity.java
+++ app/src/main/java/com/laptopapik/appdokartest/HomeActivity.java
@@ -1,18 +1,46 @@
 package com.laptopapik.appdokartest;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.os.Bundle;
-
+import android.support.design.widget.TabLayout;
 
 public class HomeActivity extends AppCompatActivity {
+    ViewPager mViewPager;
+    TabPageAdapter mAdapter;
+    TabLayout mTablayout;
+    Toolbar mytoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mytoolbar= findViewById(R.id.my_toolbar);
+        setSupportActionBar(mytoolbar);
+        getSupportActionBar().setTitle("Dokar Home");
+        mViewPager = findViewById(R.id.container);
+        mAdapter = new TabPageAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        mTablayout = findViewById(R.id.tab1);
+        mTablayout.setupWithViewPager(mViewPager);
 
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.act_bar, menu);
+        return true;
+    }
 
+    public void onGroupItemClick (MenuItem item) {
+        /*
+        One of the group items (using the onClick attribute) was clicked
+        The item parameter passed here indicates which item it is
+        All other menu item clicks are handled by onOptionsItemSelected()
+        */
+    }
 
 }
---
+++ app/src/main/java/com/laptopapik/appdokartest/TabPageAdapter.java
@@ -0,0 +1,46 @@
+package com.laptopapik.appdokartest;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+
+
+/**
+ * Created by Laptop Apik on 20/12/2017.
+ */
+
+public class TabPageAdapter extends FragmentPagerAdapter {
+    public static int PAGE_COUNT = 4;
+    private String judultab[] = new String[] {"Satu Fragment","Dua Fragment","Tiga Fragment","Empat Fragment"};
+    public TabPageAdapter (FragmentManager fm ) {super(fm);};
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0 :
+                return new FragmentSatu();
+            case 1 :
+                return new FragmentDua();
+            case 2 :
+                return new FragmentTiga();
+            case 3 :
+                return new FragmentEmpat();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return judultab[position];
+    }
+
+
+
+}
